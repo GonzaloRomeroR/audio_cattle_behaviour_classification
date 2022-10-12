25b968ef3110445f6ae36547fd815ce233905fb2
gromero <gromero@createitreal.com>
2022-10-12 21:53:08 +0200

    Update decision tree

--- esp32/components/decision_tree/decision_tree.c
+++ esp32/components/decision_tree/decision_tree.c
@@ -9,15 +9,36 @@
 
 int decision_tree_classify(float duration, float crosses, float maximum, float simetry, float desviation)
 {
-    if (desviation <= 690)
+    if (desviation <= 0.013)
     {
-        return 1;
+        if (duration <= 0.019)
+        {
+            return 1;
+        }
+        else
+        {
+            if (desviation <= 0.013)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
     else
     {
-        if (maximum <= 113.5)
+        if (crosses <= 124.5)
         {
-            return 0;
+            if (simetry <= 0.189)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
         }
         else
         {
